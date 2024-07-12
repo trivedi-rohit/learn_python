0d3f593767ffb7913beee0caf3640f2673790b31
trivedi-rohit <mr.trivedirohit@gmail.com>
2024-07-12 18:30:00 +0530

    Find maxm. number from usr list

---
+++ WAP-Maxm_Number.py
@@ -0,0 +1,12 @@
+num = input("Enter list of numbers : ")
+split_num = num.split()
+count = 0
+for i in split_num:
+    count += 1
+for i in range(0,count):
+    split_num[i] = int(split_num[i])
+max_numbr = split_num[0]
+for number in split_num:
+    if number > max_numbr:
+        max_numbr = number
+print(f"The maximum number is : {max_numbr}")

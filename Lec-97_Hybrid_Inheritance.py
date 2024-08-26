7bc5562f9751028c9e3e7410c0a3a409aaa3ca79
trivedi-rohit <mr.trivedirohit@gmail.com>
2024-08-26 13:13:00 +0530

    Update : Hybrid Inheritance

--- Lec-97_Hybrid_Inheritance.py
+++ Lec-97_Hybrid_Inheritance.py
@@ -18,3 +18,46 @@ d1 = D()
 d1.display()
 print(D.mro())
 
+#******************************** Exercise********************************
+class University():
+    def __init__(self,uni_name):
+        self.uni_name = uni_name
+    def display(self):
+        print(f'university : {self.uni_name}')
+        
+class Course(University):
+    def __init__(self,uni_name,course_name):
+        University.__init__(self,uni_name)
+        self.course_name = course_name
+    def display(self):
+        print(f'Course : {self.course_name}')
+        
+class Branch(University):
+    def __init__(self,uni_name,branch):
+        University.__init__(self,uni_name)
+        self.branch = branch
+    def display(self):
+        print(f'Branch : { self.branch} ')
+
+class Student(Course,Branch):
+    def __init__(self,uni_name,course_name,branch, student_name):
+        Course.__init__(self,uni_name,course_name)
+        Branch.__init__(self,uni_name,branch)
+        self.student_name = student_name
+    def display(self):
+        print(f"Student : {self.student_name}")
+        
+class Faculty(Branch):
+        def __init__(self,uni_name,branch):
+            Branch.__init__(self,branch)
+        def display(self):
+            print('I am from Faculty')
+# u1 = University('ABC')   
+# c1 = Course('ABC','BTECH') 
+# b1 = Branch('ABC','CSE')        
+student_1 = Student('AKTU','BTECH','ECE',"Rohit Trivedi")
+# print(Student.mro())
+student_1.display()
+University.display(student_1)
+Branch.display(student_1)
+Course.display(student_1)
